821cb73e310c664d9f7c0da0876f38436cee5fd1
msaat366 <msaat366@gmail.com>
2021-08-19 18:53:34 +0530

    initialized firebase for use

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: 'AIzaSyBJ8BIozm8m6Mh625DJyLDjKHrZi--120g',
+  authDomain: 'chat-app-3d4b8.firebaseapp.com',
+  projectId: 'chat-app-3d4b8',
+  storageBucket: 'chat-app-3d4b8.appspot.com',
+  messagingSenderId: '874785747372',
+  appId: '1:874785747372:web:c2a6c234965321bbc8ca09',
+  measurementId: 'G-W0MC2PE2VR',
+};
+
+const app = firebase.initializeApp(config);
+
