f89ba87a0e26e48e696b90f5b54440b87ca4c96a
msaat366 <msaat366@gmail.com>
2021-08-25 19:20:24 +0530

    added realtime message storing and last message displayed in sidebar

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -1,11 +1,91 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react';
+import { Alert, Icon, Input, InputGroup } from 'rsuite';
+import firebase from 'firebase/app';
+import { useParams } from 'react-router';
+import { useProfile } from '../../../context/profile.context';
+import { database } from '../../../misc/firebase';
 
-const ChatBottom = () => {
-    return (
-        <div>
-            b
-        </div>
-    )
+function assembleMessage(profile, chatId) {
+  return {
+    roomId: chatId,
+    author: {
+      name: profile.name,
+      uid: profile.uid,
+      created: profile.createdAt,
+      ...(profile.avatar ? { avatar: profile.avatar } : {}),
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+  };
 }
 
-export default ChatBottom
+const ChatBottom = () => {
+  const [input, setInput] = useState('');
+  const [isloading, setisLoading] = useState(false);
+
+  const { chatId } = useParams();
+  const { profile } = useProfile();
+
+  const onInputChange = useCallback(value => {
+    setInput(value);
+  }, []);
+
+  const onSendClick = async () => {
+    if (input.trim === '') {
+      return;
+    }
+
+    const msgData = assembleMessage(profile, chatId);
+    msgData.text = input;
+
+    const updates = {};
+
+    const messageId = database.ref('messages').push().key;
+
+    updates[`/messages/${messageId}`] = msgData;
+    updates[`/rooms/${chatId}/lastMessage`] = {
+      ...msgData,
+      msgId: messageId,
+    };
+
+    setisLoading(true);
+    try {
+      await database.ref().update(updates);
+
+      setInput('');
+      setisLoading(false);
+    } catch (err) {
+      setisLoading(false);
+      Alert.error(err.message, 4000);
+    }
+  };
+
+    const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      ev.preventDefault();
+      onSendClick();
+    }
+  };
+
+  return (
+    <div>
+      <InputGroup>
+        <Input
+          placeholder="Write a new Message here"
+          value={input}
+          onChange={onInputChange}
+          onKeyDown={onKeyDown}
+        />
+        <InputGroup.Button
+          color="blue"
+          appearance="primary"
+          onClick={onSendClick}
+          disabled={isloading}
+        >
+          <Icon icon="send" />
+        </InputGroup.Button>
+      </InputGroup>
+    </div>
+  );
+};
+
+export default ChatBottom;
--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
+import { ButtonToolbar, Icon } from 'rsuite';
+import { useCurrentRoom } from '../../../context/current-room-context';
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import RoomInfobBtnModal from './RoomInfobBtnModal';
 
 const ChatTop = () => {
-    return (
-        <div>
-            t
-        </div>
-    )
-}
+  const name = useCurrentRoom(v => v.name);
+  const isMobile = useMediaQuery('(max-width: 992px)');
 
-export default ChatTop
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4 className="text-disappear d-flex align-items-center">
+          <Icon
+            componentClass={Link}
+            to="/"
+            icon="arrow-circle-left"
+            size="2x"
+            className={
+              isMobile
+                ? 'd-inline-block p-0 mr-2 text-blue link-unstyled'
+                : 'd-none'
+            }
+          />
+          <span className="text-disappear">{name}</span>
+        </h4>
+        <ButtonToolbar className="ws-nowrap">todo</ButtonToolbar>
+      </div>
+      <div className="d-flex justify-content-between align-items-center">
+        <span>todo</span>
+        <RoomInfobBtnModal />
+      </div>
+    </div>
+  );
+};
+
+export default memo(ChatTop);
